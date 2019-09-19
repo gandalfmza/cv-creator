6a3fca8d539616d60d6946806c4773eb4288870b
Gabriel Barberà (External) <gabriel.barbera@odigeo.org>
2019-09-19 17:58:41 +0200

    Updates

---
+++ precache-manifest.ba2e61753b18619951c1ef977db50f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e009a839bc1cd16a5d87a3529b97b13c",
+    "url": "/cv-creator/index.html"
+  },
+  {
+    "revision": "67ee5e69357692b44c02",
+    "url": "/cv-creator/static/css/main.d753259d.chunk.css"
+  },
+  {
+    "revision": "0a6ddabddcd644bf8edc",
+    "url": "/cv-creator/static/js/2.146dc517.chunk.js"
+  },
+  {
+    "revision": "67ee5e69357692b44c02",
+    "url": "/cv-creator/static/js/main.cc1c6873.chunk.js"
+  },
+  {
+    "revision": "0236b62d58bff0606c37",
+    "url": "/cv-creator/static/js/runtime~main.79a2283c.js"
+  }
+]);
